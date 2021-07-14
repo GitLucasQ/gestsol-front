f2532e043331cc116cf2c7c537c8f88369d63896
Luis Carlos <lucas@localhost.localdomain>
2021-07-14 00:49:57 -0500

    Agregado grafico de tickets por usuario en vista reportes

--- src/components/Reportes.js
+++ src/components/Reportes.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Pie } from '@ant-design/charts'
 import NavigationSuper from './NavigationSuper'
 import axios from 'axios'
 
@@ -7,12 +8,29 @@ const Reportes = () => {
     const [listaPersonas, setListaPersonas] = useState([])
     const [personaSeleccionada, setPersonaSeleccionada] = useState('')
     const [estadoTicket, setEstadoTicket] = useState('')
+    const [ticketPorPersona, setTicketPorPersona] = useState([])
 
     const getListaPersonas = async () => {
         await axios.post('http://localhost:5050/listarUsuarios',
             { idUsuario: JSON.parse(sessionStorage.getItem('token')).token })
             .then((response) => {
                 setListaPersonas(response.data.usuarios)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    const getTicketsPorPersona = async () => {
+        await axios.post('http://localhost:5050/ticket/ticketsPorUsuario',
+            { id_usuario: JSON.parse(sessionStorage.getItem('token')).token })
+            .then((response) => {
+                setTicketPorPersona(
+                    response.data.resultado.map(dato => ({
+                        usuario: dato.persona,
+                        cantidad: dato.cantidad
+                    }))
+                )
             })
             .catch((error) => {
                 console.log(error)
@@ -28,8 +46,20 @@ const Reportes = () => {
     }
 
     useEffect(() => {
-        getListaPersonas();
+        //getListaPersonas();
+        getTicketsPorPersona();
     })
+
+
+    var config = {
+        appendPadding: 10,
+        data: ticketPorPersona,
+        angleField: 'cantidad',
+        colorField: 'usuario',
+        radius: 0.8,
+        label: { type: 'outer' },
+        interactions: [{ type: 'element-active' }],
+    }
 
     return (
         <div>
@@ -38,7 +68,7 @@ const Reportes = () => {
             <form>
                 <div className="form-row">
                     <div className="form-group col-md-6">
-                        <label for="selectPersona">Nombre</label>
+                        <label>Nombre</label>
                         <select
                             id="selectPersona"
                             className="form-control custom-select"
@@ -53,7 +83,7 @@ const Reportes = () => {
                         </select>
                     </div>
                     <div className="form-group col-md-6">
-                        <label for="selectEstado">Estado</label>
+                        <label>Estado</label>
                         <select
                             id="selectEstado"
                             className="form-control custom-select"
@@ -63,6 +93,9 @@ const Reportes = () => {
                             <option value="cerrado">Cerrados</option>
                         </select>
                     </div>
+                </div>
+                <div className="row">
+                    <Pie {...config} />
                 </div>
             </form>
         </div>
